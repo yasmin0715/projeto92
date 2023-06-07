3024bc5214b90c23f73946a452f1b19ab6f327c9
yasmin0715 <129914162+yasmin0715@users.noreply.github.com>
2023-06-07 20:35:14 -0300

    Add files via upload

---
+++ mainn.js
@@ -0,0 +1,33 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+
+
+
+
+
+	
+
+
+}
+
+function playSound(){
+
+}
+function new_image()
+ { fabric.Image.fromURL('AniversarioImagem.png', function(Img) { 
+    blockImageObject = Img;
+    blockImageObject.scaleToWidth(700);
+    blockImageObject.scaleToHeight(510);
+    blockImageObject.set({
+    top:0,
+    left:0 
+    });
+    canvas.add(blockImageObject);
+    });
+    }
+    function playSound(){ 
+    x.play(); 
+    }
